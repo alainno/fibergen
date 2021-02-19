6c8315b93959eb2ff4d7890544fc3c5ce01ed774
Alain <alain@gruposistemas.com>
2021-02-19 17:21:23 -0500

    new functions for regression data

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from fiberrandom import FiberSample
 import os
+import json
 
 def emptyDir(folder):
     '''
@@ -18,10 +19,14 @@ def emptyDir(folder):
             print(e)
 
 def drawFibers(sample_image, waves, min_thickness, max_thickness):
+    sum_thickness = 0
     for wave in waves:
         wave = np.array(wave).astype(int)
         wave = wave.reshape((-1,1,2))
-        cv2.polylines(sample_image,[wave],False,(255,255,255), randint(min_thickness,max_thickness), cv2.LINE_AA)
+        thickness = randint(min_thickness,max_thickness)
+        cv2.polylines(sample_image,[wave],False,(255,255,255), thickness, cv2.LINE_AA)
+        sum_thickness += thickness
+    return sum_thickness
 
 def createSamples(directory, size, samples, thickness, fibers):
     fiber_sample = FiberSample(size[0], size[1])
@@ -30,6 +35,20 @@ def createSamples(directory, size, samples, thickness, fibers):
         sample_image = np.zeros((size[0], size[1], 3), np.uint8)
         drawFibers(sample_image, waves, thickness[0], thickness[1])
         cv2.imwrite(directory + "/" + str(i + 1).zfill(4) + ".png", sample_image)
+
+def createSamplesAndMetrics(directory, size, samples, thickness, fibers):
+    fiber_sample = FiberSample(size[0], size[1])
+    metrics = {}
+    for i in range(0,samples):
+        waves = fiber_sample.createRandomWaves(randint(fibers[0],fibers[1]))
+        sample_image = np.zeros((size[0], size[1], 3), np.uint8)
+        sum_thickness = drawFibers(sample_image, waves, thickness[0], thickness[1])
+        sample_path = directory + "/" + str(i + 1).zfill(4) + ".png"
+        cv2.imwrite(sample_path, sample_image)
+        metrics[sample_path] = round(sum_thickness / len(waves), 2)
+    with open(directory + "/metrics.json", 'w') as fp:
+        json.dump(metrics, fp)
+    
 
 def initDirectory(path):
     if(os.path.isdir(path)):
