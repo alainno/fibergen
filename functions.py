406aeae66c6f733693bc22cfbcce85576ff6b694
Alain <alaings@gmail.com>
2021-04-30 11:41:54 -0500

    skeleton ground truth

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from fiberrandom import FiberSample
 import os
+import random
 
 def emptyDir(folder):
     '''
@@ -21,7 +22,22 @@ def drawFibers(sample_image, waves, min_thickness, max_thickness):
     for wave in waves:
         wave = np.array(wave).astype(int)
         wave = wave.reshape((-1,1,2))
-        cv2.polylines(sample_image,[wave],False,(255,255,255), randint(min_thickness,max_thickness), cv2.LINE_AA)
+        cv2.polylines(sample_image,[wave],False,(255,255,255),randint(min_thickness,max_thickness), cv2.LINE_AA)
+        
+def drawSampleFibers(sample_image, waves, thickness_list, skeleton=False):
+    '''
+    dibuja las fibras ondeadas sobre una imagen
+    '''    
+    for i,wave in enumerate(waves):
+        wave = np.array(wave).astype(int)
+        wave = wave.reshape((-1,1,2))
+        #thickness = randint(min_thickness,max_thickness)    
+        color = (thickness_list[i],thickness_list[i],thickness_list[i]) if skeleton else (255,255,255)
+        #cv2.polylines(sample_image,[wave],False,color,thickness_list[i],cv2.LINE_AA)
+        thickness = 1 if skeleton else thickness_list[i]
+        line = cv2.LINE_8 if skeleton else cv2.LINE_AA
+        cv2.polylines(sample_image, [wave], False, color, thickness, cv2.LINE_8)
+    
 
 def createSamples(directory, size, samples, thickness, fibers, extension="png", printout=True):
     fiber_sample = FiberSample(size[0], size[1], printout)
@@ -42,3 +58,61 @@ def initDirectory(path):
         else:
             print ("Successfully created the directory %s " % path)
 
+def createSamplesAndSkeletons(dir_samples, dir_skeletons, size, samples, thickness, fibers, extension="png", printout=True):
+    
+    fiber_sample = FiberSample(size[0], size[1], printout)
+    
+    for i in range(0,samples):
+        waves = fiber_sample.createRandomWaves(randint(fibers[0],fibers[1]))
+        
+        sample_image = np.zeros((size[0], size[1], 3), np.uint8)
+        drawFibers(sample_image, waves, thickness[0], thickness[1])
+        
+        skeleton_image = np.zeros((size[0], size[1], 3), np.uint8)
+        drawFibers(skeleton_image, waves, 1, 1)
+        
+        cv2.imwrite(dir_samples + "/" + str(i + 1).zfill(4) + "." + extension, sample_image)
+        cv2.imwrite(dir_skeletons + "/" + str(i + 1).zfill(4) + "." + extension, skeleton_image)
+        
+def createSamplesAndVariableSkeletons(
+    dir_samples,
+    dir_skeletons,
+    size,
+    total_samples,
+    thickness_range,
+    fibers_range,
+    extension="png",
+    printout=False):
+    '''
+    Create samples with respective skeletons with different values that represents fiber diameter
+    '''
+    
+    fiber_sample = FiberSample(size[0], size[1], printout)
+    
+    for i in range(total_samples):
+        
+        print(f'i={i}')
+        
+        len_waves = randint(fibers_range[0],fibers_range[1])
+        
+        fiber_waves = fiber_sample.createRandomWaves(len_waves)
+        
+        thickness_list = []
+        for _ in range(len_waves):
+            thickness_list.append(randint(thickness_range[0],thickness_range[1]))
+        #thickness_list = sorted(thickness_list)
+        
+        sample_image = np.zeros((size[0], size[1], 3), np.uint8)
+        #drawFibers(sample_image, waves, thickness[0], thickness[1])
+        drawSampleFibers(sample_image, fiber_waves, thickness_list)
+        
+        skeleton_image = np.zeros((size[0], size[1], 3), np.uint8)
+        drawSampleFibers(skeleton_image, fiber_waves, thickness_list, True)
+        
+        cv2.imwrite(dir_samples + "/" + str(i + 1).zfill(4) + "." + extension, sample_image)
+        cv2.imwrite(dir_skeletons + "/" + str(i + 1).zfill(4) + "." + extension, skeleton_image)
+        
+
+    
+
+    
